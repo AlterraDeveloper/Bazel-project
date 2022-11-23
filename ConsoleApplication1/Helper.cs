5f98fe2d9c18710e98ab99398888aa61664bac58
Eugene Kiselev <eugene.kiselev@financesoft.kg>
2022-11-23 09:50:03 +0600

    initial commit

---
+++ ConsoleApplication1/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    public static class Helper
+    {
+        public static string Help()
+        {
+            var list = new List<int> { 1,2,3,4,5 };
+            var array = list.Where(x => x % 2 == 0).ToArray();
+            var arrayAsJson = JsonConvert.SerializeObject(array);
+            return arrayAsJson;
+            // return string.Join(", ", array.Select(i => i.ToString()));
+        }
+    }
+}
